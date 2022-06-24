473f50f3af6d4c949a5261c7daee3270ee1b3128
Teo FLEMING <tfleming@e1r4p9.clusters.42paris.fr>
2022-06-24 15:52:19 +0200

    probably ready for turning in??

--- peda_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## peda
-## a plugin works with gdb
-# https://github.com/longld/peda
-
-# either this script, or the commands below needs to be executed on each level
-chmod +rwx ~
-echo "source /tmp/peda/peda.py" >> ~/.gdbinit
